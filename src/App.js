28ac6d2af22e86ceb935868f8e5ce784cd8e60b2
yomename <yomename@naver.com>
2020-02-04 23:11:59 +0900

    https://github.com/yomename/movie_app_2020.git

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
+function Food({ favorite }) {
+  return <h1>I like {favorite}</h1>;
+}
+
 function App() {
   return (
-  <div>
-    <h1>HELLO!</h1>
-  </div>
+    <div>
+      <h1>HELLO!</h1>
+      <Food favorite="kimchi" />
+      <Food favorite="ramen" />
+      <Food favorite="samguopsal" />
+      <Food favorite="chukumi" />
+    </div>
 
-  )
+  );
 
 }
 
